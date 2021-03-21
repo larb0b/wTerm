0f14fc6b88fde60cf700097b2b08481b0c3d8f6f
Larkin Nickle <me@larbob.org>
2021-03-21 05:20:26 -0400

    Phone keyboard layout: Tab, |, <, >

--- src/enyo/vkbLayouts/phone_aux.js
+++ src/enyo/vkbLayouts/phone_aux.js
@@ -8,19 +8,13 @@ enyo.kind({
 	layout: [
 		[
 			{flex:1},
-			{symbols: [['Esc',SDLK._ESCAPE]]},
-			{flex:1},
 			{symbols: [['Tab<br><img src="images/key_tab.png" class="keyImg"/>',SDLK._TAB]]},
 			{flex:1},
-			{symbols: [['<img src="images/cursorUp.png" class="keyImg"/>',SDLK._UP],null,['<span class="fnBind">PgUp</span>',SDLK._PAGEUP]], extraClasses: 'arrow'},
+			{symbols: [['|']], printable: true},
 			{flex:1},
-			{symbols: [['<img src="images/cursorDown.png" class="keyImg"/>',SDLK._DOWN],null,['<span class="fnBind">PgDn</span>',SDLK._PAGEDOWN]], extraClasses: 'arrow'},
+			{symbols: [['<']], printable: true},
 			{flex:1},
-			{symbols: [['<img src="images/cursorLeft.png" class="keyImg"/>',SDLK._LEFT],null,['<span class="fnBind">Home</span>',SDLK._HOME]], extraClasses: 'arrow'},
-			{flex:1},
-			{symbols: [['<img src="images/cursorRight.png" class="keyImg"/>',SDLK._RIGHT],null,['<span class="fnBind">End</span>',SDLK._END]], extraClasses: 'arrow'},
-			{flex:1},
-			{symbols: [['Sym',SDLK._MENU]], extraClasses: 'sym'},
+			{symbols: [['>']], printable: true},
 			{flex:1}
 		]
 	]
